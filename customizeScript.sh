99ed4d977e41541f059a81be860403f8e016d18d
danielcgonzalez <100765557+danielcgonzalez@users.noreply.github.com>
2022-03-02 14:57:53 +0100

    Update customizeScript.sh

--- customizeScript.sh
+++ customizeScript.sh
@@ -11,3 +11,27 @@ for user in `awk -F: '($3 < 500) {print $1 }' /etc/passwd`; do
         fi 
     fi
 done
+
+# Establecer el grupo por defecto para la cuenta root:
+usermod -g 0 root
+
+# Establecer permisos
+echo "umask 077" >>  /etc/bashrc
+echo "umask 077" | tee -a /etc/profile.d/*
+
+# Bloquear las cuentas de los usuarios inactivos mediante la ejecución de la siguiente instrucción:
+useradd -D -f 35
+
+# parámetros de configuración de las contraseñas
+sed -i 's/^PASS_MAX_DAYS.*/PASS_MAX_DAYS 45/g' /etc/login.defs
+sed -i 's/^PASS_MIN_DAYS.*/PASS_MIN_DAYS 1/g' /etc/login.defs
+sed -i 's/^PASS_MIN_DAYS.*/PASS_WARN_AGE 7/g' /etc/login.defs
+
+for user in `awk -F: '($3 < 500) {print $1 }' /etc/passwd`; do 
+ if [ $user != ""root"" ] && [ $user != ""sync"" ] && [ $user != ""shutdown"" ] && [ $user != ""halt"" ] 
+ then 
+ chage --maxdays 45 $user
+ chage --mindays 1 $user
+ chage --warndays 7 $user
+ fi
+done
