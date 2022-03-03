42d9b45b07fe2ee1ef6ba90aebe0f4272aaf4f54
danielcgonzalez <100765557+danielcgonzalez@users.noreply.github.com>
2022-03-03 15:50:34 +0100

    Update customizeScript.sh

--- customizeScript.sh
+++ customizeScript.sh
@@ -144,7 +144,20 @@ if [ -f "$FILE" ]; then
     chmod 600 $FILE 
 fi
 
+# Bastionado
+FILE=/etc/modprobe.d/bastionado.conf 
+    echo "install cramfs /bin/true" > $FILE 
+    echo "install freevxfs /bin/true" >> $FILE 
+    echo "install jffs2 /bin/true" >> $FILE 
+    echo "install hfs /bin/true" >> $FILE 
+    echo "install hfsplus /bin/true" >> $FILE 
+    echo "install squashfs /bin/true" >> $FILE 
+    echo "install udf /bin/true" >> $FILE 
 
+    echo "install dccp /bin/true" >> $FILE
+    echo "install sctp /bin/true" >> $FILE
+    echo "install rds /bin/true" >> $FILE
+    echo "install tipc /bin/true" >> $FILE
 
 # Arranque
 FILE=/etc/grub.conf
