43eaa2f2838d46eaf4420a500c96e3a318ec2216
danielcgonzalez <100765557+danielcgonzalez@users.noreply.github.com>
2022-03-08 15:56:58 +0100

    Update customizeScript.sh

--- customizeScript.sh
+++ customizeScript.sh
@@ -305,21 +305,3 @@ sudo apt-get install -y logrotate
 
 # Desactivar servicio Rsync
 systemctl disable rsync.service
-
-# Desactivar paquete landscape
-sudo apt-get remove landscape-common  -y
-
-FILE=/etc/security/pwquality.conf
-if [ -f "$FILE" ]; then
-    echo "minlen = 14" >> $FILE
-    echo "minclass = 4" >> $FILE
-fi
-
-# borrar usuario "games"
-sudo userdel games
-
-
-sed -i 'SYS_UID_MIN        100' /etc/login.defs
-sed -i 'SYS_UID_MAX        999' /etc/login.defs
-
-
