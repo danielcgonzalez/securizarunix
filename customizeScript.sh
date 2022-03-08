6f66aaa71e58664fd0c487367d52fec028dccd0c
danielcgonzalez <100765557+danielcgonzalez@users.noreply.github.com>
2022-03-08 18:55:50 +0100

    Update customizeScript.sh

--- customizeScript.sh
+++ customizeScript.sh
@@ -325,9 +325,3 @@ sudo userdel games
 echo "SYS_UID_MIN        100" >> /etc/login.defs
 echo "SYS_UID_MAX        999" >> /etc/login.defs
 
-#password grub
-echo 'set superusers="root"' >> /etc/grub.d/40_custom
-# Ejemplo pass: seguridadgrub
-# se genera asi: grub-mkpasswd-pbkdf2
-echo 'password_pbkdf2 root grub.pbkdf2.sha512.10000.325C6A44CD4D13931BC8D2C88E198BB4B7384DC04983E487E78FDD137B4451B3B6514375CD34B77D25CAA0902E287124EAF34008B91490662F418B70967EB4E2.9F63BD7C97933438F0215167182E435007A871871BE091BFEB15BC2A2F62F3F6CDDCA730D7B3AEFBAFD1F3839CADF09D8CECA2AF80C4C48BB9C3AB9D7D3994BE' >> /etc/grub.d/40_custom
-grub-mkconfig -o /boot/grub/grub.cfg
